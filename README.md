d8c64f7baac8c4240769efba0d096b188356cf4c
younes.ouarhim@student-cs.fr <ouarhim_you@centralesupelec.fr>
2025-06-07 11:41:35 +0200

    files deleted

--- README.md
+++
@@ -1,2 +0,0 @@
-# Claims-Drift-Open-DAMIR
-Analysis of claims drift in the French healthcare sector using Open DAMIR database
--- utils.py
+++
@@ -1,7 +0,0 @@
-import os 
-
-parent_path = os.path.dirname("/raid/datasets/allianzsante/")
-for page in range(1, 7):
-    path = os.path.join(parent_path, f"page{page}")
-    if not os.path.exists(path):
-        os.makedirs(path)
