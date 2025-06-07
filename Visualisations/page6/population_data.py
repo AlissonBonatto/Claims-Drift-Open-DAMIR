bf5cf6cee0d3d36c14ad0e1e31b30d3dea521cf7
Alisson Bonatto <alissonbonatto@hotmail.com>
2025-06-07 20:26:37 +0200

    added population_data.py file

---
+++ Visualisations/page6/population_data.py
@@ -0,0 +1,42 @@
+dict_region_population_2023 = {
+    "Auvergne-Rhône-Alpes": 8235923,
+    "Bourgogne-Franche-Comté": 2791719,
+    "Bretagne": 3453023,
+    "Centre-Val de Loire": 2573295,
+    "Provence-Alpes-Côte d'Azur et Corse": 5553539,
+    "Grand Est": 5568711,
+    "Hauts-de-France - Nord-Pas-de-Calais-Picardie": 5983823,
+    "Ile-de-France": 12419961,
+    "Normandie": 3327077, 
+    "Aquitaine-Limousin-Poitou-Charentes": 6154772,
+    "Pays de la Loire": 3926389,
+    "Languedoc-Roussillon-Midi-Pyrénées": 6154729,
+    "Régions et Départements d'outre-mer": 2230472
+}
+
+dict_region_population_2024 = {
+    "Auvergne-Rhône-Alpes": 8260096,
+    "Bourgogne-Franche-Comté": 2793080,
+    "Bretagne": 3475895,
+    "Centre-Val de Loire": 2581479,
+    "Provence-Alpes-Côte d'Azur et Corse": 5601749,
+    "Grand Est": 5544051,
+    "Hauts-de-France - Nord-Pas-de-Calais-Picardie": 5973933,
+    "Ile-de-France": 12450849,
+    "Normandie": 3341312, 
+    "Aquitaine-Limousin-Poitou-Charentes": 6191209,
+    "Pays de la Loire": 3936719,
+    "Languedoc-Roussillon-Midi-Pyrénées": 6201587,
+    "Régions et Départements d'outre-mer": 2253657
+}
+
+dict_age_population_2024 = {
+    0: 15.734e6,
+    20: 7.850e6,
+    30: 8.368e6,
+    40: 8.509e6,
+    50: 8.924e6,
+    60: 8.243e6,
+    70: 10.976e6*0.6,      #Extrapolation
+    80: 10.976e6*0.4       #Extrapolation
+}
